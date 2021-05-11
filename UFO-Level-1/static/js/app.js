07d182cc19d3c6af32f97cd16548665298d9d70a
aschultz10 <77344570+aschultz10@users.noreply.github.com>
2021-05-11 12:14:59 -0400

    button looping complete

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -13,19 +13,38 @@ var filtersearch = d3.select("datetime");
 var button = d3.select(".btn-default");
 
 // Append table with for each code
-data.forEach((alien) => {
+data.forEach((sighting) => {
     var row = tbody.append("tr");
 
     // Use link
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
-    Object.entries(alien).forEach(([key, value]) => {
+    Object.entries(sighting).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
 });
 
 //  Button loop for search bar
-//  Need to filter using datetime aka filtersearch
+//  Need to filter using datetime, filtersearch
+button.on("click", function() {
+  var date = d3.select("#datetime").property("value");
+  filtered = tableData
+  if (date) {
+      filtered = filtered.filter((row) => row.datetime === date);
+  };
+
+//console log, tbody
+      console.log(filtered);
+      tbody.html("");
 
 
+// filter within button, foreach just like above append TR and TD
+    filtered.forEach((filter) => {
+        var row = tbody.append("tr");
+        Object.entries(filter).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+          });
+      });
+  });
 // YOUR CODE HERE
